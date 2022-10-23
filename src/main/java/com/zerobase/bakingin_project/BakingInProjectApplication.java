65e50bcfcd6edb2b18a5bf612ef757d2b690a639
eunjung0203 <grace99985@gmail.com>
2022-10-24 00:05:47 +0900

    first-commit

---
+++ src/main/java/com/zerobase/bakingin_project/BakingInProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.bakingin_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakingInProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakingInProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/bakingin_project/GithubActionTest.java
@@ -0,0 +1,13 @@
+package com.zerobase.bakingin_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class GithubActionTest {
+    @Test
+    void githubActionTest() {
+        assertEquals(1, 1);
+    }
+}
