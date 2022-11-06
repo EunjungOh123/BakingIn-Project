60481c402ce0d7a6726eb7b1b03e690c4062e0aa
eunjung0203 <grace99985@gmail.com>
2022-11-07 00:12:21 +0900

    레시피 게시판 조회수 기능 추가 (쿠키 사용하여 중복 방지)

--- src/main/java/com/zerobase/bakingin_project/board/repository/BoardRepository.java
+++ src/main/java/com/zerobase/bakingin_project/board/repository/BoardRepository.java
@@ -2,7 +2,13 @@ package com.zerobase.bakingin_project.board.repository;
 
 import com.zerobase.bakingin_project.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    @Modifying
+    @Query("update Board b set b.views = b.views + 1 where b.id = :id")
+    int updateView(@Param("id") Long id);
 }
